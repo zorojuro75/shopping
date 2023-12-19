f5c2a6527a5ff75eb3d2a315f9aa536bf3962af9
zorojuro <smasayedalbanna75@gmail.com>
2023-12-19 18:10:17 +0600

    navbar

--- shopping/components/Product.tsx
+++ shopping/components/Product.tsx
@@ -10,10 +10,10 @@ type Props = {
 const Product = (props: Props) => {
   return (
     <div className="bg-white flex flex-col rounded-md hover:shadow-xl h-96 pt-5">
-      <div className="h-44 bg-black bg-opacity-10 flex justify-center items-center mx-4">
-        <Image src={props.path} alt="next" width={100} height={1} />
+      <div className="h-44 bg-black bg-opacity-10 flex justify-center items-center mx-4 overflow-clip">
+        <Image src={props.path} alt="next" width={120} height={1} />
       </div>
-      <div className="text-2xl font-bold capitalize text-center flex-1 mx-4 flex flex-col justify-center items-center gap-2 py-2">
+      <div className="text-2xl font-bold capitalize text-center flex-1 mx-4 flex flex-col justify-center items-center gap-2 py-2 overflow-x-clip">
         <div>{props.name}</div>
         <div className="text-sm font-thin text-center">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, molestias aliquam. Pariatur.
